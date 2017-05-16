b7afcc496f8aead32d633ce254aa67cdb74c6f67
DimaNovik <dimanovik1989@gmail.com>
2017-05-16 16:01:05 +0300

    Start project - Displaing Weather on React. Created button with cities and connect to API.

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const PLACES = [
+  {name: "Odessa", zip: "65000"},
+  {name: "Kiyv", zip: " 01000"},
+  {name: "Lviv", zip: "79000"},
+  {name: "Poltava", zip: "36000"},
+  {name: "Dnipro", zip: "49000"},
+];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace : 0,
+    }
+  }
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div className="App">
+        {PLACES.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({activePlace: index});
+            }}
+          >
+            {place.name}
+          </button>
+        ))}
+         <WeatherDisplay 
+          key={activePlace}
+          zip={PLACES[activePlace].zip} 
+          />
+      </div>
+    );
+  }
+}
+
+class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip + 
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData : json});
+    });
+  }
+  render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p></p>
+      </div>
+    );
+  }
+}
+
+export default App;
